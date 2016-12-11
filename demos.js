51b93096308a5eab74f6b7426db9e7801f709524
Romel Pérez <ronelprhone@gmail.com>
2016-12-10 23:47:35 -0500

    [chore] demos

---
+++ demos.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 9800;
+
+app.use(express.static(__dirname));
+
+app.listen(port, function (err) {
+  if (err) throw err;
+  console.log(`Server running at http://127.0.0.1:${port}`);
+});
