8e674099e27a7be1b2a3edfaf10ab969236a2c41
Romel Pérez <ronelprhone@gmail.com>
2016-12-13 21:28:13 -0500

    [feat] reducers space

---
+++ src/core/reducers/space/index.js
@@ -0,0 +1,25 @@
+import { Map } from 'immutable';
+import { ACTIONS } from 'consts';
+
+const getInitialState = function () {
+  return Map();
+};
+
+const reducer = function (state, action) {
+
+  if (!state) {
+    return getInitialState();
+  }
+
+  switch (action.type) {
+
+    case ACTIONS.CON.UPDATE: {
+      return state.merge(action.payload);
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
