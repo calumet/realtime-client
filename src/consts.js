58bf9026e2031980751d3365338f6a19ad327dc8
Romel Pérez <ronelprhone@gmail.com>
2016-12-17 17:32:11 -0500

    [feat] consts

---
+++ src/consts.js
@@ -0,0 +1,29 @@
+export default {};
+
+export const ACTIONS = {
+  USERS: {
+    RESET: 0,
+  },
+  USERSCATEGORIES: {
+    RESET: 0,
+  },
+  SPACE: {
+    UPDATE: 0,
+  },
+  SPACEROOMS: {
+    RESET: 0,
+    AVAILABILITY: 1,
+  },
+  ROOMSUSERS: {
+    RESET: 0,
+  },
+  ROOMSMSGS: {
+    ADD: 0,
+    MERGE: 1,
+  },
+  CON: {
+    ADD: 0,
+    REMOVE: 1,
+    MERGE: 2,
+  },
+};
