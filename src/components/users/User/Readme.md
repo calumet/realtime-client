c99969f1b42c8f6a8a41eb29a4ee402d14cb6462
Romel Pérez <ronelprhone@gmail.com>
2016-12-21 21:31:11 -0500

    [feat] component users user

---
+++ src/components/users/User/Readme.md
@@ -0,0 +1,12 @@
+Usuario sin foto.
+
+    <User name='Romel Pérez' category='Ingeniero de Software' />
+
+Usuario con foto y conectado.
+
+    <User
+      name='Romel Pérez'
+      category='Ingeniero de Software'
+      photo='https://avatars3.githubusercontent.com/u/1393135?v=3&s=460'
+      online={true}
+    />
---
+++ src/components/users/User/index.js
@@ -0,0 +1,54 @@
+import React, { PropTypes } from 'react';
+import Dotdotdot from 'react-dotdotdot';
+import classNames from 'classnames';
+
+/**
+ * Usuario de la comunicación.
+ */
+export default function User (props) {
+
+  const { name, category, photo, online, className, ...rest } = props;
+  const cls = classNames('users-user', className, {
+    'users-user--online': online
+  });
+
+  const photoStyle = {};
+  if (photo) {
+    photoStyle.backgroundImage = `url(${photo})`;
+  }
+
+  return (
+    <div {...rest} className={cls}>
+      <div className='users-user__row'>
+        <div className='users-user__col users-user__col1'>
+          <div className='users-user__photo' style={photoStyle} />
+        </div>
+        <div className='users-user__col users-user__col2'>
+          <Dotdotdot clamp={1}>
+            <div className='users-user__name'>{name}</div>
+          </Dotdotdot>
+          <Dotdotdot clamp={1}>
+            <div className='users-user__cat'>{category}</div>
+        </Dotdotdot>
+        </div>
+        <div className='users-user__col users-user__col3'>
+          <div className='users-user__status'></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  category: PropTypes.string,
+  photo: PropTypes.string,
+  online: PropTypes.bool,
+};
+
+User.defaultProps = {
+  name: '',
+  category: '',
+  photo: '',
+  online: false,
+};
